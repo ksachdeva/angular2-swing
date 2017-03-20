b45a13ae2f1021e8257d891cf93ec0bff35c23ea
Sparkomatic <jason.sparks.uk@icloud.com>
2017-03-20 10:28:16 +0000

    removing dist folder

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { SwingStackComponent } from './swing-stack-component';
-export { SwingCardComponent } from './swing-card-component';
-export { ThrowDirection, ThrowEvent, DragEvent, ThrowEventName, DragEventName, Card, Stack, StackConfig, Direction } from './swing';
-export { SwingModule } from './swing.module';
